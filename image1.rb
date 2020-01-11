fe2a5153df377c50db868f9047ece2382f73d100
Happy8-8 <gafonji@gmail.com>
2020-01-11 01:01:11 +0000

    file updated

--- image1.rb
+++ image1.rb
@@ -1,20 +1,63 @@
 class Image
-  attr_accessor :data
+  attr_accessor :image
 
-  def initialize(data)
-    self.data = data
+  def initialize(image)
+    @image = image
   end
 
   def output_image
-    self.data.each {|x| puts x.join}
+    @image.each {|x| puts x.join}
   end
 
 end
 
+def blur_coords!
+
+  blur_coords = []
+
+  @image.each_with_index do |row, i|
+
+    row.each_with_index do |x, row_i|
+
+      blur_coords << [i, row_i] if x == 1
+
+  end
+
+end
+
+blur_coords.each do |coord|
+
+      @image[coord[0]][coord[1] + 1] = 1 if coord[1] + 1 <= @image[coord[0]].length - 1
+
+      @image[coord[0]][coord[1] - 1] = 1 if coord[1] - 1 >= 0
+
+      @image[coord[0] + 1][coord[1]] = 1 if coord[0] + 1 <= @image.length - 1
+
+      @image[coord[0] - 1][coord[1]] = 1 if coord[0] - 1 >= 0
+
+  end
+
+end
+
+end
+
+
 image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
+
+  [0, 0, 0, 0, 0, 0],
+
+  [0, 1, 0, 0, 0, 0],
+
+  [0, 0, 0, 0, 0, 0],
+
+  [0, 0, 0, 0, 0, 0],
+
+  [0, 0, 0, 0, 0, 1],
+
+  [0, 0, 0, 0, 0, 0]
+
 ])
+
+image.blur_coords!
+
 image.output_image
